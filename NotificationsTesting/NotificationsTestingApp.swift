5fbfc4a78b411a4bcf4f72bdcaaa9bd04c4f22b4
Garrett Keyes <garrett.keyes@gmail.com>
2025-12-21 12:25:25 -0600

    Initial Commit

---
+++ NotificationsTesting/NotificationsTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsTestingApp.swift
+//  NotificationsTesting
+//
+//  Created by Garrett Keyes on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
